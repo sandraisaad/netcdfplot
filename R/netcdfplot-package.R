5c197ac52cb79d74d68182b03ebe878f67aa7571
Sandra Saad <sandra.saad@ufcg.edu.br>
2021-04-29 05:18:02 -0300

    Melhoria na documentacao

--- R/netcdfplot-package.R
+++ R/netcdfplot-package.R
@@ -14,6 +14,37 @@
 #' plota_mapa(temperatura_tempo1, vlon = longitudes, vlat = latitudes, mapa_brasil= T, mapa_amsul = T, paleta = "YlGnBu")
 #' media_area(temperatura_tempo1, mapa = "Brasil",vlat = latitudes, vlon = longitudes,fun = "max")
 #'
+#' #Carrega o arquivo do Netcdf, as dimensoes (longitude, latitude e tempo) e a variavel
+#'
+#'gfs <- carrega_modelo("/data/gfs_2019092500mod.nc");
+#'
+#'longitudes <- carrega_longitude(gfs);
+#'
+#'latitudes <- carrega_latitude(gfs);
+#'
+#'date <- carrega_tempo(gfsmodel)
+#'
+#'temperatura <- carrega_variavel(gfs, "tmp2m");
+#'
+#' #Plota o mapa da temperatura no tempo 1 e calcula o valor maximo na area
+#'
+#'temperatura_tempo1 <- temperatura[,,1];
+#'
+#'plota_mapa(temperatura_tempo1, vlon = longitudes, vlat = latitudes, mapa_brasil = T, mapa_amsul = T, paleta = "YlGnBu");
+#'
+#'media_area(temperatura_tempo1, mapa = "Brasil", vlat = latitudes, vlon = longitudes, fun = "max")
+#'
+#'#### Plota uma serie temporal de um ponto
+#'
+#'coordx <- lon2x(longitudes,-46.5)
+#'
+#'coordy <- lat2y(latitudes,-23.5)
+#'
+#'temperatura_ponto <- temperatura[x = coordx, y = coordy, ]
+#'
+#'df_temperatura_ponto<-data.frame(date, temperatura_ponto)
+#'
+#'plot(df_temperatura_ponto)
 #' @docType package
 #' @name netcdfplot
 NULL
